b51f8fb759fdec53c6d18cb00786396a031d9849
Alex <project.demonio@gmail.com>
2015-12-12 13:56:44 -0500

    Adding mongo to the mix

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sed -i 's/FRONTEND_SERVER/'"$FRONTEND_SERVER"'/g' /etc/nginx/nginx.conf
+echo "sleeping while the dependencies load https://github.com/docker/compose/issues/374"
+sleep 15;
 nginx -g "daemon off;"
