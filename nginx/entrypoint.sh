044deaa288f779d70c866b4097b7a308694dfc05
Alex <project.demonio@gmail.com>
2016-03-05 23:34:57 -0500

    Moving nginx info to another repo

--- nginx/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sed -i 's/FRONTEND_SERVER/'"$FRONTEND_SERVER"'/g' /etc/nginx/nginx.conf
-sed -i 's/PRERENDER_TOKEN/'"$PRERENDER_TOKEN"'/g' /etc/nginx/nginx.conf
-echo "sleeping while the dependencies load https://github.com/docker/compose/issues/374"
-sleep 15;
-nginx -g "daemon off;"
