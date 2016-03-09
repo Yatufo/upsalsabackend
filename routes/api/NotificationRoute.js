2f7d2c87210c6288e9cec5b5bbf5d426eb8f8b26
Alex <project.demonio@gmail.com>
2016-03-09 08:56:32 -0500

    Adding notification when posting

---
+++ routes/api/NotificationRoute.js
@@ -0,0 +1,24 @@
+var email 	= require("emailjs");
+
+var server 	= email.server.connect({
+   user:     "admin@upsalsa.com",
+   password: "sAlsA123!",
+   host:     "smtp.zoho.com",
+   port:     465,
+   ssl:      true
+});
+
+
+exports.notify = function(req, res, next) {
+
+  var message	= {
+     text:	JSON.stringify(req.body),
+     from:	"No Reply <admin@upsalsa.com>",
+     to:		"upsalsa@gmail.com",
+     subject:	"Notification POST" + req.originalUrl
+  };
+
+  // send the message and get a callback with an error or details of the message that was sent
+  server.send(message, function(err, message) { console.log(err || message); });
+  next();
+}
